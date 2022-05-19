31549100d2050500bb7879e3dfd043e36b1a5795
saman <s.saman@iitg.ac.in>
2022-05-19 11:52:56 +0530

    changes related to update using id

--- excel_upload/upload_app/serializers.py
+++ excel_upload/upload_app/serializers.py
@@ -6,4 +6,16 @@ class ExcelDataSerializer(serializers.ModelSerializer):
         model = ExcelData
         fields = ['id','name','age','hobby','date_created']
 
+    def update(self, instance, validated_data):
+        excel_data=instance
+
+        if "name" in validated_data:
+            excel_data.name = validated_data["name"]
+        if "age" in validated_data:
+            excel_data.age = validated_data["age"]
+        if "hobby" in validated_data:
+            excel_data.hobby = validated_data["hobby"]
+
+        excel_data.save()
+        return excel_data.id
     
--- excel_upload/upload_app/views.py
+++ excel_upload/upload_app/views.py
@@ -2,7 +2,7 @@ from hashlib import new
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from .serializers import ExcelDataSerializer
 from tablib import Dataset
 from .models import ExcelData
 
@@ -18,17 +18,26 @@ class UploadApi(APIView):
         try:
             dataset = Dataset()
             new_excel = request.FILES['myfile']
-            print(new_excel.name)
             imported_data = dataset.load(new_excel.read(),format='xlsx')
 
             for data in imported_data:
-                excel_data=ExcelData(name=data[0],age=int(data[1]),hobby=data[2])
-                excel_data.save()
+                if data[0]:
+                    excel_data = ExcelData.objects.filter(id=int(data[0])).first()
+                    if excel_data:
+                        serializer = ExcelDataSerializer(instance=excel_data, data={'name':data[1],'age':int(data[2]),'hobby':data[3]}, partial=True)
+                        if serializer.is_valid(raise_exception=True):
+                            serializer.save()       
+                        else:
+                            error = next(iter(serializer.errors))
+                            response["messagae"] = serializer.errors[str(error)][0]
+                            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    excel_data=ExcelData(name=data[1],age=int(data[2]),hobby=data[3])
+                    excel_data.save()
 
             response['message'] = "data base created using excel data"
-            return Response(response, status=status.HTTP_200_OK)
-                     
-           
+            return Response(response, status=status.HTTP_201_CREATED)
+
         except Exception as e:
             response['message'] = str(e)
             response['status'] = False
